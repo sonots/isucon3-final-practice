c3c20d08dcfbbee29149e2a17ad6fd83ec0bc434
sonots <sonots@gmail.com>
2014-11-02 13:25:19 +0000

    benched (image on redis)

--- bench/http_stat.rb
+++ bench/http_stat.rb
@@ -2,30 +2,31 @@ def parse_ltsv(line)
   # line.split("\t").map {|a| a.split(":", 2) }.to_h
   Hash[*(line.split("\t").map {|a| a.split(":", 2) }.flatten)]
 end
-
+ 
 def stat(lines)
   stats = {}
   lines.each do |line|
     ltsv = parse_ltsv(line)
     reqtime = ltsv["reqtime"].to_f
-    uri   = ltsv["uri"]
-    # if uri =~ /image/ or uri =~ /icon/
-    #   uri = File.dirname(uri)
-    # end
-    stat = stats[uri] ||= { :count => 0, :sum => 0, :min => 0, :max => 0}
+    uri = ltsv["uri"]
+    if uri =~ /\/image\// or uri =~ /\/icon\//
+      uri = File.dirname(uri)
+    end
+    method = ltsv["method"]
+    stat = stats["#{method} #{uri}"] ||= { :count => 0, :sum => 0, :min => 0, :max => 0}
     stat[:sum] += reqtime
     stat[:max] = [stat[:max], reqtime].max
     stat[:min] = [stat[:min], reqtime].min
     stat[:count] += 1
   end
-
+ 
   stats.each do |uri, stat|
     stat[:mean] = stat[:sum] / stat[:count].to_f
     out = { "uri" => uri }.merge(stat)
     puts out.map {|key, v| "#{key}:#{v}" }.join("\t")
   end
 end
-
+ 
 lines = STDIN.readlines
 request_lines = lines.grep(/\turi:.*reqtime:/)
 stat(request_lines)
